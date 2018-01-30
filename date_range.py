ff3b86cc962fa238f4b278c5ccf4af53c626739e
mlampri <35842961+mlampri@users.noreply.github.com>
2018-01-30 15:14:17 +0200

    Add files via upload

---
+++ date_range.py
@@ -0,0 +1,16 @@
+data = open('data.csv')
+
+for line in data:
+
+    # split the line up
+    columns = line.rstrip('\n').split(',')
+
+
+    # assign the columns to variables
+    title = columns[0]
+    year = int(columns[1])
+    type = columns[2]
+    length = int(columns[3])
+
+    if 1839 <= year <= 1845:
+        print(title)
